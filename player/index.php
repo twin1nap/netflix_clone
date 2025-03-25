607666cde75587cdb2ddbd10d2a904759d458afc
Joran Nap <143212549+twin1nap@users.noreply.github.com>
2025-03-25 13:38:30 +0100

    push 1

---
+++ player/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico">
+</head>
+<body>
+    <video src="" id="player"></video>
+</body>
+</html>
